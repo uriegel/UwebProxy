dc53c465d758aa15bce3b5d1900803c6837963ca
Uwe Riegel <uriegel@hotmail.de>
2020-03-15 11:27:49 +0100

    Tester and solution

---
+++ Tester/Program.fs
@@ -0,0 +1,10 @@
+﻿let configuration = Configuration.create {
+    Configuration.createEmpty() with 
+        Port = 9865
+        Requests = [ ]
+}
+
+let server = Server.create configuration 
+server.start ()
+stdin.ReadLine() |> ignore
+server.stop ()
